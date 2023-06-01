4d60a4ff920f3bc466137d825249f87f5593c93a
Pranish <pranishpoudel01@gmail.com>
2023-06-01 18:08:47 +0545

    fix:

--- funinput.py
+++ funinput.py
@@ -3,3 +3,10 @@ def greet_with_name(name):
 
 
 greet_with_name("Pranish")
+
+#two input
+def greet_with_addy(name,add):
+    print(f"Your name is {name} and address is {add}")
+
+
+greet_with_addy("insa","Bagar-1, Pokhara")
