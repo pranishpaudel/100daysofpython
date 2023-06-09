a9822218d3cf50cdc66d519de4ba7364d24cb93a
Pranish <pranishpoudel01@gmail.com>
2023-06-09 18:11:13 +0545

    feat: :sparkles: Fixbuzz

--- fixbug.py
+++ fixbug.py
@@ -14,4 +14,15 @@ def my_function():
       print("You got it")
 my_function()
 
+
 #since inside loop it excludes the last value
+
+
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list) 
+ 
+mutate([1,2,3,5,8,13])
