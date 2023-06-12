f29325824871b2c02db4d5da818e8b69684e95df
Pranish <pranishpoudel01@gmail.com>
2023-06-12 19:48:23 +0545

    feat: :sparkles: OOPSTART

---
+++ OOP.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+my_screen= Screen()
+
+
+timmy= Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+timmy.speed("slow")
+
+print(my_screen.canvheight)
+my_screen.exitonclick()
