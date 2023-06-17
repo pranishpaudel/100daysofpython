e807370d9c1c0d167a4b7585b78ff53dabfa10fb
Pranish <pranishpoudel01@gmail.com>
2023-06-17 19:41:41 +0545

    feat: :sparkles: TimmyTutleChallenge1

---
+++ timmytutlev1.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+timmy_the_turtle= Turtle()
+
+timmy_the_turtle.shape("arrow")
+timmy_the_turtle.color("red","yellow")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
