d4fb39902c20ffd6c1e61175396542d9a346454b
Pranish <pranishpoudel01@gmail.com>
2023-06-28 22:16:45 +0545

    feat: :bug: Snake Game

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,48 +1,28 @@
-
-
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
-screen= Screen()
 
+screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
+screen.title("My Snake Game")
 screen.tracer(0)
 
-positioning= [(0,0),(-20,0), (-40,0)]
+snake = Snake()
+food = Food()
 
-segments= []
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-
-
-for postion in positioning:
-    segment= Turtle("square")
-    segment.color("white")
-    segment.penup()
-    segment.goto(postion)
-    segments.append(segment)
-
-is_game_on= True
-
-while is_game_on:
-
+game_is_on = True
+while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segments) -1 , 0, -1):
-       new_x= segments[seg_num - 1].xcor()
-       new_y= segments[seg_num - 1].ycor()
-       segments[seg_num].goto(new_x, new_y)
 
-    segments[0].forward(20)
-    segments[0].left(20)
-    segments[0].forward(20)
-
-
-
-
+    snake.move()
 
 
 screen.exitonclick()
-
-
-  
