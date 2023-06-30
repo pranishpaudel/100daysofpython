bbf719e94990e42996591f713e268092f0bb7b78
Pranish <pranishpoudel01@gmail.com>
2023-06-30 21:45:41 +0545

    feat: :sparkles: Simple Turtle

---
+++ simpleturtle.py
@@ -0,0 +1,26 @@
+import turtle
+
+# Create a turtle screen
+screen = turtle.Screen()
+
+# Create a turtle
+my_turtle = turtle.Turtle()
+
+# Move the turtle forward and turn right
+my_turtle.forward(100)
+my_turtle.right(90)
+
+# Move the turtle forward and turn right again
+my_turtle.forward(100)
+my_turtle.right(90)
+
+# Move the turtle forward and turn right again
+my_turtle.forward(100)
+my_turtle.right(90)
+
+# Move the turtle forward and turn right again to complete the square
+my_turtle.forward(100)
+my_turtle.right(90)
+
+# Close the turtle graphics window
+turtle.done()
