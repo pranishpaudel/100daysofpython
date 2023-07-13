42c3ef21b749898148ba5bdc92a3eefe8049dc37
Pranish <pranishpoudel01@gmail.com>
2023-07-13 23:12:31 +0545

    fix: :zap: Main

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen= Screen()
@@ -9,12 +11,21 @@ screen.title("My snake game")
 screen.tracer(0)
 
 
+
 snake= Snake()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
+
+
+
+
+food= Food()
+scoreboard= Scoreboard()
+
+
 
 is_game_on= True
 
@@ -23,5 +34,11 @@ while is_game_on:
     time.sleep(0.1)
 
     snake.move_snake()
+
+    if snake.head.distance(food)<15:
+       
+       food.refresh()
+       scoreboard.increase()
+    
 
 screen.exitonclick()
