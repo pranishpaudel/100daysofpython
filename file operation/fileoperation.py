f5936b58e00735d8ac8e391dcc5dfe45bf2a0dca
Pranish <pranishpoudel01@gmail.com>
2023-07-17 21:38:36 +0545

    feat: :bug: File Operator

---
+++ file operation/fileoperation.py
@@ -0,0 +1,12 @@
+with open("my_file.txt", mode="a") as file:
+    content= file.write("still to do xd")
+    content= file.write("still to do xd")
+    print(content)
+
+
+with open("new_txt.txt", mode="w") as file:
+    
+    content= file.write("naya honitaaa")
+    
+
+      
