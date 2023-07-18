0785472748ce98124abdc740d6b3a8ef785306fe
Pranish <pranishpoudel01@gmail.com>
2023-07-18 14:23:53 +0545

    feat: :sparkles: Mail Merge Project

--- file operation/fileoperation.py
+++ file operation/fileoperation.py
@@ -1,10 +1,7 @@
-with open("my_file.txt", mode="a") as file:
-    content= file.write("still to do xd")
-    content= file.write("still to do xd")
-    print(content)
 
 
-with open("new_txt.txt", mode="w") as file:
+
+with open("/Users/air/Desktop/new_txt.txt", mode="a") as file:
     
     content= file.write("naya honitaaa")
     
