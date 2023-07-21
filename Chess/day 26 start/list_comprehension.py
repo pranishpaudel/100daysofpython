550e595a8849f0f7af1702ea6fa34260f76f3c92
Pranish <pranishpoudel01@gmail.com>
2023-07-21 14:03:46 +0545

    feat: :sparkles: List Comprehension

---
+++ Chess/day 26 start/list_comprehension.py
@@ -0,0 +1,12 @@
+name= "Angela"
+
+new_list= [n*2 for n in range(1,5) if n!=2]
+
+old_list= ["innannnn", "samirasn", "Lito", "insa","Vindasd"]
+
+new_list= [name.upper() for name in old_list if len(name)>5]
+
+print(new_list)
+
+
+
