ddca5d8eb9849052046116c7a6eb40be2fcc308d
Pranish <pranishpoudel01@gmail.com>
2023-07-24 19:42:55 +0545

    feat: :sparkles: GUI

--- day 27 start/main.py
+++ day 27 start/main.py
@@ -1,12 +1,33 @@
-import tkinter
-window= tkinter.Tk()
+from tkinter import *
+
+
+
+
+clickss= "HI"
+window= Tk()
 
 window.title("My first GUI")
 
 window.minsize(width= 500, height=300)
 
-label= tkinter.Label(text="I am a label", font=("Arial",24,"bold"))
+
+label= Label(text=clickss, font=("Arial",24,"bold"))
 label.pack()
+
+
+
+def button_clicked():
+    new_text= input.get()
+    label["text"]= new_text
+
+button= Button(text="Click Me", command= button_clicked, background="yellow")
+button.pack()
+
+
+input= Entry(width=10)
+
+input.pack()
+print(input.get())
 
 
 window.mainloop()
