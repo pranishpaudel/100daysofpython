0afa4a2832a0f06bcb03a67bf93ad4d04f90fd7c
Pranish <pranishpoudel01@gmail.com>
2023-08-03 20:34:42 +0545

    feat: :sparkles: Errorhandle

--- conname.py
+++
@@ -1,9 +0,0 @@
-name1 = input("Enter the first name: ")
-name2 = input("Enter the second name: ")
-
-concatenated_names = name1 + name2
-common_letters = set(name1.lower()) & set(name2.lower())
-num_common_letters = len(common_letters)
-
-print("Concatenated names:", concatenated_names)
-print("Number of common letters:", num_common_letters)
