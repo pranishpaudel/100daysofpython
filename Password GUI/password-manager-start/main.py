00e189ea73d5d8c74edd987149d028d37953ef99
Pranish <pranishpoudel01@gmail.com>
2023-08-04 21:55:37 +0545

    feat: :zap: Added search functionality to Password GUI

--- Password GUI/password-manager-start/main.py
+++ Password GUI/password-manager-start/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import string
 import random
+import json
 desired_length=10
 
 
@@ -12,15 +13,32 @@ def add_it():
     website_entryy= website_entry.get()
     password_entryy= password_entry.get()
     email_entryy= email_entry.get()
+    new_dict= {
+        website_entryy: {
+        "Email": email_entryy, 
+        "Password": password_entryy,
+    }
+    }
 
     if not website_entryy.strip() or not password_entryy.strip() or not email_entryy.strip():
      messagebox.showinfo(title="Oops", message= "Please make sure you haven't made any fields empty")
     else:
         is_ok= messagebox.askokcancel(title=website_entryy, message=f"These are the details entered: \n {website_entryy} |{email_entryy} | {password_entryy} \n Is it okay to save?")
         if is_ok:
-                file= open("Password GUI/password-manager-start/savedata.txt", mode= "a")
-                file.write(f"{website_entryy} |{email_entryy} | {password_entryy} \n")
-                file.close()
+                #READING OLD DAATA
+
+                try:
+                    with open("Password GUI/password-manager-start/data.json", mode= "r") as file:
+                        data= json.load(file)
+                        data.update(new_dict)
+                        
+                        with open("Password GUI/password-manager-start/data.json", mode= "w") as file:
+                         json.dump(data, file, indent=4)
+                except:
+                    with open("Password GUI/password-manager-start/data.json", mode= "w") as file:
+                        json.dump(new_dict, file, indent=4)
+                
+
 
 
 # ----------------\----------- UI SETUP ------------------------------- #
@@ -29,6 +47,8 @@ def generate_random_password(length=10):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     password_entry.insert(0,password)
+
+
 
 
 
@@ -52,8 +72,8 @@ passoword_label= Label(text="password")
 passoword_label.grid(row=3, column=0)
 
 #entry
-website_entry= Entry(width=35)
-website_entry.grid(row=1,column=1, columnspan=2)
+website_entry= Entry(width=21)
+website_entry.grid(row=1,column=1)
 website_entry.focus()
 email_entry= Entry(width=35)
 email_entry.grid(row=2,column=1, columnspan=2)
@@ -66,5 +86,20 @@ generate_pass_button= Button(text="Generate Password",command=generate_random_pa
 generate_pass_button.grid(row=3, column=2)
 add_button= Button(text="Add",width=36, command=add_it)
 add_button.grid(row=4, column=1, columnspan=2)
+
+def search_item():
+    website= website_entry.get()
+    with open("Password GUI/password-manager-start/data.json") as file:
+        data= json.load(file)
+        if website in data:
+            info= data[website]
+            email= info["Email"]
+            password= info["Password"]
+            messagebox.showinfo(title=website, message=f"Email: {email} || Pass: {password}")
+    
+search_button= Button(text="Search", command=search_item)
+search_button.grid(row=1, column=2, columnspan=2)
+
+
 
 window.mainloop()
