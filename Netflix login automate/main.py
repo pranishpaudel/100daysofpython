77ea21932a6a130a5c8dbfccba49d8226c76febc
Pranish <pranishpoudel01@gmail.com>
2023-10-06 12:34:57 +0545

    fix: :bug: Fix a bug

--- Netflix login automate/main.py
+++ Netflix login automate/main.py
@@ -4,68 +4,66 @@
 import time
 
 from netflix_actions import Netflix
-USER_EMAIL= "storevideo77@gmail.com"
-USER_PASS= "@thenew56"
+USER_EMAIL= "bedul.ol123@gmail.com"
+USER_PASS= ""
+NEW_PASS= "sad32dasdas"
 PROFILE_PIN= "4242"
 
-PROFILE_TO_PIN= "alex siss"
+PROFILE_TO_PIN= "swoyam"
 IS_MASTER_PROFILE= True
 
+INCORRECT_PASS_RES= "Incorrect password"
+TECHNICAL_MSG="We are having technical difficulties"
+WELCOME_MSG= "Welcome back"
 
-# ENDPOINT= "https://www.netflix.com/np/login"
-# AFTER_LOGIN_ENDPOINT= "https://www.netflix.com/YourAccount"
-# options= webdriver.ChromeOptions()
-# options.add_experimental_option("detach",True)
-# driver= webdriver.Chrome(options)
-# driver.get(ENDPOINT)
-# netflix_email= driver.find_element(By.NAME,"userLoginId")
+netflix_data= open("Netflix login automate/netflixaccs.txt")
 
-# netflix_email.send_keys(USER_EMAIL)
+netflix_accounts= []
+netflix_accounts=netflix_data.read().strip().split("\n")
 
-# netflix_pass= driver.find_element(By.NAME,"password")
+for netflixx in netflix_accounts:
 
-# netflix_pass.send_keys(USER_PASS)
+    netflix= Netflix(n_Email=netflixx.split("|")[0], n_Password=netflixx.split("|")[1])
+    driver_message= netflix.login()
+    if WELCOME_MSG.lower() in netflix.check_alive_netflix().lower():
+        print(f"Netflix: THE NETFLIX ACCOUNT {netflixx} HAS DIED")
+    elif not INCORRECT_PASS_RES.lower() in driver_message.lower() and not TECHNICAL_MSG.lower() in driver_message.lower():
+        netflix.redir_next_page()
+
+        time.sleep(0.9)
+        returned_full_info, returned_all_profiles = netflix.extract_all_info()
+        print(f"Netflix: {netflixx} || Full info: {returned_full_info} || Profiles {returned_all_profiles}")
+        try:
+            PROFILE_TO_PIN_FORMATTED= f"profile_{returned_all_profiles.index(PROFILE_TO_PIN.lower())}"
+        except:
+            print("Input Profile not found")
+            exit()
+        all_date, all_title= netflix.retrive_latest_watch(sprofile_name=PROFILE_TO_PIN_FORMATTED)
+        if not all_date==[1,5,6]:
+            formatted_title_list=[]
+            number_of_watches= len(all_date)
+            in_value=0
+            while not number_of_watches==0:
+                formatted_title_list.append(f"Date: {all_date[in_value].text} || Title: {all_title[in_value].text}")
+                number_of_watches-=1
+                in_value+=1
+
+            print(formatted_title_list)
+            netflix.close_driver()
+        else:
+            print("Error printing the watch history")
+        # netflix.change_password(old_pass=NEW_PASS,new_pass="ASCIIEBCDIC1")
+        
+        # netflix.put_pin(profile_name=PROFILE_TO_PIN_FORMATTED,account_password=USER_PASS,pin_code=PROFILE_PIN)
+
+    else:
+        print(f"Netflix: {netflixx} || Driver Message: {driver_message}")
 
 
-# sign_in_button= driver.find_element(By.XPATH,'//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/form/button')
 
-# sign_in_button.click()
-# time.sleep(2)
-# driver.get(AFTER_LOGIN_ENDPOINT)
-# time.sleep(2)
+# PROFILE_TO_PIN_FORMATTED= f"profile_{returned_all_profiles.index(PROFILE_TO_PIN.lower())}"
 
-# last_four_digits= driver.find_element(By.CSS_SELECTOR,".account-section-item .wallet--mop").text[15:]
-# card_brand= driver.find_element(By.CSS_SELECTOR,".account-section-item .wallet--mop img").get_attribute("alt")
-# print(last_four_digits)
-# print(card_brand)
-
-# account_payment_plan= driver.find_element(By.XPATH,'//*[@id="appMountPoint"]/div/div/div/div[2]/div/div/div[6]/div[2]/section/div/div[1]/div[1]/div/b').text
-
-# account_status= driver.find_element(By.XPATH,'//*[@id="appMountPoint"]/div/div/div/div[2]/div/div/div[6]/div[1]/section/div[2]/div/div/div[1]/div[2]').text
-# print(account_status)
-# print(account_payment_plan)
-
-# profile_names= driver.find_elements(By.CSS_SELECTOR,".profile-hub ul .single-profile")
-
-# all_profiles= []
-# for profiles in profile_names:
-#    all_profiles.append(profiles.text.split("\n")[0])
-
-
-
-netflix= Netflix(n_Email=USER_EMAIL, n_Password=USER_PASS)
-netflix.login()
-time.sleep(2)
-netflix.redir_next_page()
-
-time.sleep(2)
-returned_full_info, returned_all_profiles = netflix.extract_all_info()
-print(returned_full_info)
-print(returned_all_profiles)
-
-PROFILE_TO_PIN_FORMATTED= f"profile_{returned_all_profiles.index(PROFILE_TO_PIN.lower())}"
-
-print(PROFILE_TO_PIN_FORMATTED)
+# print(PROFILE_TO_PIN_FORMATTED)
 
 
 
