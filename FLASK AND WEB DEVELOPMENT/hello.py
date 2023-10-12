20a48d6b0228c8008b924f97a1e4a79ad88a0fb3
Pranish <pranishpoudel01@gmail.com>
2023-10-12 20:36:37 +0545

    feat: Advanced Decorator

--- FLASK AND WEB DEVELOPMENT/hello.py
+++ FLASK AND WEB DEVELOPMENT/hello.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/bye')
+def bye():
+    return "BYW!!!!!"
+
+
+@app.route('/username/<user>/<int:number>')
+def heelllo(user,number):
+    return f"Hello there {user} with number {number}"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
