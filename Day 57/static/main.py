5481f1ccc116953aa8d71d72e725c1edd7ac09bf
Pranish <pranishpoudel01@gmail.com>
2023-10-16 21:20:40 +0545

    feat: :sparkles: Added try for error codes

--- Day 57/static/main.py
+++ Day 57/static/main.py
@@ -20,8 +20,10 @@ def get_data_by_name(name):
 def home(namee):
     random_num= randint(0,10)
     current_year= datetime.now().year
-    return render_template("index.html",numm=random_num,year=current_year,age=get_data_by_name(namee),name=namee)
+    new_name= namee[0].upper()+namee[1:]
+    return render_template("index.html",numm=random_num,year=current_year,age=get_data_by_name(namee),name=new_name)
 
+@app.route("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
