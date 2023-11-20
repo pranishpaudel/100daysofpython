4abaa8e092a9ff0537d2bc6a95eb157360e17b20
Pranish <pranishpoudel01@gmail.com>
2023-11-20 12:56:13 +0545

    perf: :ambulance: Added Some advanced data sorting with Pandas

--- Day 73 - Data exploration with Pandas/main.py
+++ Day 73 - Data exploration with Pandas/main.py
@@ -10,7 +10,7 @@ print(clean_db["Undergraduate Major"].loc[post])
 
 
 
-#What college major has the highest mid-career salary? How much do graduates with this major earn? 
+#What college major has the highest mid-career salary? How much do graduates with this major earn?
 
 mid_range_id_max= clean_db["Mid-Career Median Salary"].idxmax()
 Mid_Career_Median_Salary= clean_db["Undergraduate Major"].loc[mid_range_id_max]
@@ -28,13 +28,53 @@ print(GRADUATES1_EARNING)
 
 
 
-#How would we calculate the difference between the earnings of the 10th and 90th percentile? 
+#How would we calculate the difference between the earnings of the 10th and 90th percentile?
 
 
 diff_db= clean_db['Mid-Career 90th Percentile Salary'] - clean_db['Mid-Career 10th Percentile Salary']
 
 clean_db.insert(1,"Spread",diff_db)
-print(clean_db.head())
+# print(clean_db.head())
+
+
+#To see which degrees have the smallest spread, we can use the .sort_values() method. 
+low_risk= clean_db.sort_values("Spread",ascending=False)
+
+# print(low_risk[['Undergraduate Major','Spread']].head())
+
+
+
+# find the degrees with the highest potential? Find the top 5 degrees with the highest values in the 90th percentile. 
+
+
+
+ninthith_per= clean_db.sort_values("Mid-Career 90th Percentile Salary",ascending=False)
+top_5_ninthith_per= ninthith_per["Undergraduate Major"].head()
+
+print("\n")
+#PRINTING OUT THE ACTUAL MAJORS WITH HIGHEST 90th PERCENTILE
+for top_majors in top_5_ninthith_per:
+  print(top_majors)
+
+#find the degrees with the greatest spread in salaries. Which majors have the largest difference between high and low earners after graduation.
+
+spread_per= clean_db.sort_values("Spread",ascending=False)
+top_spread_per= spread_per["Undergraduate Major"].head()
+print("\n")
+#PRINTING OUT THE ACTUAL MAJORS WITH HIGHEST SPREAD
+for top_majorss in top_spread_per:
+  print(top_majorss)
+
+
+print("\n")
+
+count_major= clean_db.groupby("Group").count()["Mid-Career Median Salary"]
+print(count_major)
+print("\n")
+count_major_salary= clean_db.groupby("Group")["Starting Median Salary"].mean().idxmax()
+print(count_major_salary)
+
+
 
 
 
