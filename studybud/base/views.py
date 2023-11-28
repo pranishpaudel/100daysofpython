13a1449e94e77dc2f405e4635b87e2f6f1b6afa0
Pranish <pranishpoudel01@gmail.com>
2023-11-28 20:25:41 +0545

    perf: :zap: Added Django Based ORM and ModelForms

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
+from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 from django.http import HttpResponse
 
+rooms =[
+    {'id':1, 'name':'Lets Learn Python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend Developers'},
 
+]
 def home(request):
-    return HttpResponse("Home Page")
+    rooms= Room.objects.all
+    return render(request,'base/home.html',{'rooms': rooms})
 
-def room(request):
-    return HttpResponse("ROOM")
+def room(request, pk):
+    room=Room.objects.get(id=pk)
+    content= {'room':room}
+    return render(request, 'base/room.html', content)
+
+
+def create_room(request):
+    form= RoomForm()
+    context= {'form':form}
+    return render(request,'base/room_form.html',context)
