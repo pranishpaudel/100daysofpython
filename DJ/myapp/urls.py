eab685c4780ace760f660102bdf65d9e167a4c87
Pranish <pranishpoudel01@gmail.com>
2023-12-02 17:54:13 +0545

    perf: :zap: Added some new ORM

--- DJ/myapp/urls.py
+++ DJ/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('project/<pk>/', views.project,name="project"),
+    path('create_project/', views.create_project,name="create_project"),
     path('', views.projects,name="home"),
 ]
