f1c63b58ec35f99e8ca2152b20b7bd315af1650b
Pranish <pranishpoudel01@gmail.com>
2023-12-03 15:01:37 +0545

    perf: :zap: Added Django Modelform

--- DJ/myapp/views.py
+++ DJ/myapp/views.py
@@ -1,13 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import Project,Tag
+from .forms import ProjectForm
 from django.views.decorators.http import require_http_methods,require_POST,require_GET
 # Create your views here.
 
 
 def projects(request):
-    return HttpResponse(f"Here are our projects")
+    projects= Project.objects.all()
+    context= {'projects':projects }
+    return render(request,"projects.html", context)
 
 
 
 def project(request,pk):
-    return HttpResponse('SINGLE PROJECT'+' '+str(pk))
+    
+    projectObj= Project.objects.get(id=pk)
+    tags= projectObj.tags.all()
+    context= {'projectObj':projectObj,'tags':tags}
+    return render(request,"single-project.html",context)
+
+
+
+def create_project(request):
+    form= ProjectForm()
+    if request.method=="POST":
+        form=ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(projects)
+
+
+    return render(request,'project_form.html',{'form':form})
+
+
+
+def update_project(request):
+    form= ProjectForm()
+    if request.method=="POST":
+        form=ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(projects)
+
+
+    return render(request,'project_form.html',{'form':form})
