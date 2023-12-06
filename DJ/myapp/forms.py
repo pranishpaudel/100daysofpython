26274a4c858eea2270e43d9a63fa84abb1c5a6a5
Pranish <pranishpoudel01@gmail.com>
2023-12-06 17:42:44 +0545

    feat: :sparkles: Added Django Signals

--- DJ/myapp/forms.py
+++ DJ/myapp/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 class ProjectForm(ModelForm):
 
@@ -7,3 +8,15 @@ class ProjectForm(ModelForm):
         model= Project
         fields= '__all__'
         exclude= ["vote_total","vote_ratio"]
+
+        widgets= {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    
+    def __init__(self,*args,**kwargs):
+        super(ProjectForm,self).__init__(*args,**kwargs)
+
+        self.fields['title'].widget.attrs.update(
+            {'class': 'input', 'placeholder': "Add titlte"}
+        )
---
+++ DJ/users/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save,post_delete
+from django.dispatch import receiver
+import uuid
+
+# Create your models here.
+class Profile(models.Model):
+
+    user= models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
+    name= models.CharField(max_length=200,blank=True,null=True)
+    username= models.CharField(max_length=200,blank=True,null=True)
+    location= models.CharField(max_length=200,blank=True,null=True)
+    email= models.EmailField(max_length=500,blank=True,null=True)
+    short_intro= models.CharField(max_length=200,blank=True,null=True)
+    bio= models.TextField(blank=True,null=True)
+    profile_image= models.ImageField(null=True,blank=True,upload_to='profiles/',default="profiles/user-default.png")
+    social_github= models.CharField(max_length=200,blank=True,null=True)
+    social_linkedin= models.CharField(max_length=200,blank=True,null=True)
+    social_youtube= models.CharField(max_length=200,blank=True,null=True)
+    social_website= models.CharField(max_length=200,blank=True,null=True)
+    created= models.DateTimeField(auto_now_add=True)
+    id= models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
+
+    def __str__(self):
+        return str(self.user.username)
+    
+
+
+
+class Skill(models.Model):
+
+    owner= models.ForeignKey(Profile,on_delete=models.CASCADE,null=True,blank=True)
+    name= models.CharField(max_length=200,blank=True,null=True)
+    description_for_skill= models.TextField(blank=True,null=True)
+    created= models.DateTimeField(auto_now_add=True)
+    id= models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)   
+
+    def __str__(self):
+        return str(self.name)
+
+# @receiver(post_save,sender=Profile)
+def CreateUser(sender,instance,created,**kwargs):
+     if created:
+         user= instance
+         profile= Profile.objects.create(
+             user= user,
+             username= user.username,
+             email= user.email,
+             name= user.first_name,
+         )
+
+def deleteUser(sender,instance,**kwargs):
+    print("deleting user...")
+    
+post_save.connect(CreateUser,sender=User)
+
+# post_delete.connect(deleteUser,sender=Profile)
