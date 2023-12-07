afde9ca1b91b4d6f061f3ee134cd98b0ff807f96
Pranish <pranishpoudel01@gmail.com>
2023-12-07 19:07:17 +0545

    perf: :zap: Added Login decorator

--- DJ/users/urls.py
+++ DJ/users/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('users/', views.profiles,name="profiles"),
     path('profile_user/<pk>/', views.profile_user,name="profile_user"),
+    path('login_user', views.login_user,name="login_user"),
+    path('logout_user', views.logout_user,name="logout_user"),
 ]
--- DJ/users/views.py
+++ DJ/users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate,logout
+from .models import User
 from .models import Profile,Skill
 
 # Create your views here.
@@ -18,3 +20,35 @@ def profile_user(request,pk):
 
     context= {'profile': profile,'skills': Skill.objects.all(),'topskills':topskills,'otherskills':otherskills}
     return render(request,'users/user-profile.html',context)
+
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect("profiles")
+    if request.method=="POST":
+        username= request.POST.get("username")
+        password= request.POST.get("password")
+        print(username)
+        print(password)
+
+        try:
+            user= User.objects.get(username=username)
+            print("User eXists")
+        except:
+            print("Username does not exist")
+
+        user= authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('profiles')
+    
+    
+
+    return render(request,"users/login_register.html")
+
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("home")
+
