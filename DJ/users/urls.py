5cca3036844ddd8b1a3e6d5ad6a69058f481364e
Pranish <pranishpoudel01@gmail.com>
2023-12-09 16:58:22 +0545

    feat: :sparkles: Added UserProfile

--- DJ/users/urls.py
+++ DJ/users/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('users/', views.profiles,name="profiles"),
     path('profile_user/<pk>/', views.profile_user,name="profile_user"),
     path('login_user', views.login_user,name="login_user"),
+    path('register_user', views.registerUser,name="register_user"),
     path('logout_user', views.logout_user,name="logout_user"),
+    path('account', views.userAccount,name="userAccount"),
 ]
--- DJ/users/views.py
+++ DJ/users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login, authenticate,logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 from .models import User
 from .models import Profile,Skill
+from django.contrib import messages
 
 # Create your views here.
 def profiles(request):
@@ -21,8 +24,17 @@ def profile_user(request,pk):
     context= {'profile': profile,'skills': Skill.objects.all(),'topskills':topskills,'otherskills':otherskills}
     return render(request,'users/user-profile.html',context)
 
+@login_required(login_url='login_user')
+def userAccount(request):
+    profile= request.user.profile
+    skills= profile.skill_set.all()
+    context={'profile':profile,'skills':skills}
+    return render(request,"users/account.html",context)
+
 
 def login_user(request):
+    page= True
+    mariko=False
     if request.user.is_authenticated:
         return redirect("profiles")
     if request.method=="POST":
@@ -33,22 +45,59 @@ def login_user(request):
 
         try:
             user= User.objects.get(username=username)
+            mariko= True
             print("User eXists")
         except:
-            print("Username does not exist")
-
-        user= authenticate(request,username=username,password=password)
-        if user is not None:
-            login(request,user)
-            return redirect('profiles')
+            messages.error(request,"Username does not exist")
+        if mariko:
+            user= authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('profiles')
+            else:
+                messages.error(request,"Username or password does not match")
     
     
 
-    return render(request,"users/login_register.html")
+    return render(request,"users/login_register.html",{'page':page})
 
 
 
 def logout_user(request):
     logout(request)
     return redirect("home")
+
+
+def registerUser(request):
+    page = False
+    user_exists = False
+    form = CustomUserCreationForm()
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        
+        # Check if the username already exists
+        username = request.POST.get("username")
+        if User.objects.filter(username=username).exists():
+            user_exists = True
+
+        if not user_exists:
+            if form.is_valid():
+                form.save()
+                user = authenticate(username=username, password=request.POST.get("password1"))
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, "User account created and logged in successfully.")
+                    return redirect('profiles')
+                else:
+                    messages.error(request, "Failed to authenticate user after registration.")
+            else:
+                messages.error(request, "Form is invalid. Please check the provided information.")
+        else:
+            messages.error(request, "Username already exists in the database.")
+
+    context = {'page': page, 'form': form}
+    return render(request, "users/login_register.html", context)
+
+
 
