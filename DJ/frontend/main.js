43d3f6b6e3ea6f0b96eddb57425444219a406518
Pranish <pranishpoudel01@gmail.com>
2023-12-23 10:27:45 +0545

    fix: :bug:

---
+++ DJ/frontend/main.js
@@ -0,0 +1,36 @@
+let projectsUrl= 'http://127.0.0.1:8000/api/projects/'
+
+let getProjects= () => {
+    fetch(projectsUrl)
+    .then(response => response.json())
+    .then(data=> {
+        console.log(data)
+        buildProjects(data)
+    })
+}
+
+
+
+let buildProjects= (projects) => {
+    let projectsWrapper= document.getElementById('projects--wrapper')
+    for (let i=0;projects.length>i;i++){
+let project= projects[i]
+let projectCard= `
+                  <div class="project--card">
+                  <img src="http://127.0.0.1:8000${project.featured_img}">
+                  <div>
+                  <div class="card--header">
+                  <h3>${project.title}</h3>
+                  <strong class="vote--option">&#x2719;
+                  </strong>
+                  <strong class="vote--option">&#x2212;</strong>
+                  </div>
+                  <i>${project.vote_ratio}% Postive feedback</i>
+                  <p>${project.description.substring(0,150)}</p>
+                  </div>
+                  </div>
+`
+projectsWrapper.innerHTML +=projectCard
+    }
+}
+getProjects()
